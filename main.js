28925777d18e9a5e9ca585e80800ab10b60ad0b8
Dwight Millar <millar.dwight@gmail.com>
2019-11-13 15:33:53 -0800

    fixed responsiveness so chessboard will always be in the center

--- main.js
+++ main.js
@@ -38,14 +38,14 @@ function calculateWindowDimensions() {
 function loadBoard() {
   const main = $("main");
   const columns = ['a','b','c','d','e','f','g','h'];
+  console.log(squareHeight);
   if(main.children().length) {
-    console.log('entered conditional');
-    $(".row").css("max-height", squareHeight);
+    $(".row").css("max-height", squareHeight, "width", (squareHeight * 8) + 12);
     $(".square").css("max-width", squareWidth);
     return;
   }
   for(let rowCount = 1; rowCount <= 8; rowCount++) {
-    let row = $(`<div id=${columns[rowCount - 1]} class=row style=min-height:62.5px;max-height:${squareHeight}px;></div>`)
+    let row = $(`<div id=${columns[rowCount - 1]} class=row style=min-height:62.5px;max-height:${squareHeight}px;width:${(squareHeight * 8) + 12}px></div>`)
     main.append(row);
     for(let squareCount = 1; squareCount <= 8; squareCount++) {
       row.append(`<div class=square style=min-width:62.5px;max-width:${squareWidth}px;></div>`)
