077a40570868239226f27f54b761a7a9cb62c635
Dwight Millar <millar.dwight@gmail.com>
2019-11-13 17:34:25 -0800

    created method for square class to move pieces

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var windowWidth;
 var squareWidth;
 var squareHeight;
 
+var turn = 1;
+
 
 $(document).ready(function(){
   calculateWindowDimensions();
@@ -38,6 +40,10 @@ function calculateWindowDimensions() {
 function loadBoard() {
   const main = $("main");
   const columns = ['a','b','c','d','e','f','g','h'];
+  const pieces = ['rook','knight','bishop','queen','king','bishop','knight','rook'];
+  var piece;
+  var color;
+  var player;
 
   if(main.children().length) {
     $(".row").css("max-height", squareHeight, "width", (squareHeight * 8) + 12);
@@ -49,8 +55,8 @@ function loadBoard() {
     let row = $(`<div class=row style=min-height:62.5px;max-height:${squareHeight}px;width:${(squareHeight * 8) + 12}px></div>`)
     main.append(row);
     for(let squareCount = 1; squareCount <= 8; squareCount++) {
-      console.log(squareCount);
-      var color;
+      player = '';
+      piece = '';
 
       if (rowCount % 2 === 1 && squareCount % 2 === 0) {
         color = 'black';
@@ -61,7 +67,19 @@ function loadBoard() {
       } else if (rowCount % 2 === 0 && squareCount % 2 === 1) {
         color = 'black';
       }
-      row.append(new Square(`${columns[rowCount - 1]}` + `${squareCount}`, color));
+
+      if (rowCount === 2 || rowCount === 7) {
+        piece = 'pawn';
+      } else if (rowCount === 1 || rowCount === 8) {
+        piece = pieces[squareCount - 1];
+      }
+
+      if (rowCount === 1 || rowCount === 2) {
+        player = 1;
+      } else if (rowCount === 7 || rowCount === 8) {
+        player = 2;
+      }
+      row.append(new Square(`${columns[rowCount - 1]}` + `${squareCount}`, color, piece, player));
     }
   }
 }
@@ -73,8 +91,41 @@ class Square {
     this.piece = piece,
     this.player = player
 
+    this.move = function() {
+      console.log('click');
+      if (this.player === turn) {
+        switch (this.piece) {
+          case 'pawn': {
+            null;
+          } break;
+          case 'rook': {
+            null;
+          } break;
+          case 'knight': {
+            null;
+          } break;
+          case 'bishop': {
+            null;
+          } break;
+          case 'queen': {
+            null;
+          } break;
+          case 'king': {
+            null;
+          } break;
+        }
+        if (this.player === 1) {
+          turn = 2;
+        } else {
+          turn = 1;
+        }
+      }
+    }
+
     return(
-      $(`<div id=${this.id} class="square ${this.color}${this.piece}${this.player}" style=min-width:62.5px;></div>`)
+      $(`<div id=${this.id}
+      class="square ${this.color} ${this.piece} ${this.player}"
+      style=min-width:62.5px;></div>`).click(this.move)
     )
   }
 }
