9320ba8018803555b95f26be9a6c1271793d484b
Dwight Millar <millar.dwight@gmail.com>
2019-11-13 16:46:55 -0800

    added square class with color parameter

--- main.js
+++ main.js
@@ -38,17 +38,43 @@ function calculateWindowDimensions() {
 function loadBoard() {
   const main = $("main");
   const columns = ['a','b','c','d','e','f','g','h'];
-  console.log(squareHeight);
+
   if(main.children().length) {
     $(".row").css("max-height", squareHeight, "width", (squareHeight * 8) + 12);
     $(".square").css("max-width", squareWidth);
     return;
   }
   for(let rowCount = 1; rowCount <= 8; rowCount++) {
-    let row = $(`<div id=${columns[rowCount - 1]} class=row style=min-height:62.5px;max-height:${squareHeight}px;width:${(squareHeight * 8) + 12}px></div>`)
+
+    let row = $(`<div class=row style=min-height:62.5px;max-height:${squareHeight}px;width:${(squareHeight * 8) + 12}px></div>`)
     main.append(row);
     for(let squareCount = 1; squareCount <= 8; squareCount++) {
-      row.append(`<div class=square style=min-width:62.5px;max-width:${squareWidth}px;></div>`)
+      console.log(squareCount);
+      var color;
+
+      if (rowCount % 2 === 1 && squareCount % 2 === 0) {
+        color = 'black';
+      } else if (rowCount % 2 === 1 && squareCount % 2 === 1) {
+        color = 'white';
+      } else if (rowCount % 2 === 0 && squareCount % 2 === 0) {
+        color = 'white';
+      } else if (rowCount % 2 === 0 && squareCount % 2 === 1) {
+        color = 'black';
+      }
+      row.append(new Square(`${columns[rowCount - 1]}` + `${squareCount}`, color));
     }
+  }
+}
+
+class Square {
+  constructor(id, color, piece='', player='') {
+    this.id = id,
+    this.color = color,
+    this.piece = piece,
+    this.player = player
+
+    return(
+      $(`<div id=${this.id} class="square ${this.color}${this.piece}${this.player}" style=min-width:62.5px;></div>`)
+    )
   }
 }
