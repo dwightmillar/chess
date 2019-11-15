d9d12d0d8fe01895c42912bc85ada04fb4d4078c
Dwight Millar <millar.dwight@gmail.com>
2019-11-15 12:33:04 -0800

    added images and selectors for pieces

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var squareWidth;
 var squareHeight;
 
 var turn = 1;
-
+var activePiece = '';
+var targetSquares = [];
 
 $(document).ready(function(){
   calculateWindowDimensions();
@@ -39,7 +40,7 @@ function calculateWindowDimensions() {
 
 function loadBoard() {
   const main = $("main");
-  const columns = ['a','b','c','d','e','f','g','h'];
+  const columns = ['A','B','C','D','E','F','G','H'];
   const pieces = ['rook','knight','bishop','queen','king','bishop','knight','rook'];
   var piece;
   var color;
@@ -75,9 +76,9 @@ function loadBoard() {
       }
 
       if (rowCount === 1 || rowCount === 2) {
-        player = 1;
+        player = 'white';
       } else if (rowCount === 7 || rowCount === 8) {
-        player = 2;
+        player = 'black';
       }
       row.append(new Square(`${columns[rowCount - 1]}` + `${squareCount}`, color, piece, player));
     }
@@ -91,41 +92,51 @@ class Square {
     this.piece = piece,
     this.player = player
 
-    this.move = function() {
-      console.log('click');
-      if (this.player === turn) {
-        switch (this.piece) {
-          case 'pawn': {
-            null;
-          } break;
-          case 'rook': {
-            null;
-          } break;
-          case 'knight': {
-            null;
-          } break;
-          case 'bishop': {
-            null;
-          } break;
-          case 'queen': {
-            null;
-          } break;
-          case 'king': {
-            null;
-          } break;
+    this.move = function(event) {
+      console.log(event.currentTarget.id)
+      if(activePiece === '') {
+        activePiece = event.currentTarget.id;
+
+        if (this.player === turn) {
+          switch (this.piece) {
+            case 'rook': {
+
+              null;
+            } break;
+            case 'knight': {
+              null;
+            } break;
+            case 'bishop': {
+              null;
+            } break;
+            case 'queen': {
+              null;
+            } break;
+            case 'king': {
+              null;
+            } break;
+            default: {
+              if (this.player === 1) {
+
+                turn = 2;
+              } else {
+                turn = 1;
+              }
+            }
+          }
         }
-        if (this.player === 1) {
-          turn = 2;
-        } else {
-          turn = 1;
-        }
+      } else {
+        targetSquare = event.currentTarget.id;
       }
     }
 
     return(
       $(`<div id=${this.id}
-      class="square ${this.color} ${this.piece} ${this.player}"
-      style=min-width:62.5px;></div>`).click(this.move)
+              class="square"
+              style=min-width:62.5px;background-color:${this.color};>
+              <div class="${this.piece} ${this.player}">
+              </div>
+        </div>`).click(this.move)
     )
   }
 }
