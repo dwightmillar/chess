100cc0fafd406c1ac7a80227239b0fb49ff53ab9
Dwight Millar <millar.dwight@gmail.com>
2019-11-21 03:39:18 -0800

    updated diagonal movement functions, bishops and queens now movable

--- Square.js
+++ Square.js
@@ -52,10 +52,55 @@ class Square {
           null;
         } break;
         case 'bishop': {
-          null;
+          let squareCount = 1;
+          while (CheckDownLeftMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckDownRightMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckUpLeftMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckUpRightMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
         } break;
         case 'queen': {
-          null;
+          let squareCount = 1;
+          while (CheckDownLeftMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckDownRightMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckUpLeftMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckUpRightMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          while (CheckUpMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckDownMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckRightMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
+          squareCount = 1;
+          while (CheckLeftMove(squareCount, activePiece)) {
+            ++squareCount;
+          }
         } break;
         case 'king': {
           null;
@@ -249,47 +294,140 @@ function CheckDownMove(spaces, activePiece) {
 function CheckDownRightMove(spaces, activePiece) {
   const file = activePiece.id.split('')[0];
   const rank = parseInt(activePiece.id.split('')[1]);
+  let opponent = '';
+  if (activePiece.player === 'white') {
+    opponent = 'black';
+  } else {
+    opponent = 'white';
+  }
 
-  if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`)) {
-    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).children[0].classList[0] === 'black') {
-      possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`);
-      $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).addClass('available');
+  if (activePiece.piece === 'pawn') {
+    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`)) {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).children[0].classList[0] === opponent) {
+        possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`);
+        $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).addClass('available');
+        return false;
+      }
+    }
+  } else {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`)) {
+        if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).children[0].classList[0] === activePiece.player) {
+          return false;
+        } else if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).children[0].classList[0] === opponent) {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).addClass('available');
+          return false;
+        } else {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank + spaces}`).addClass('available');
+          return true;
+        }
+      }
     }
   }
-}
+
 
 function CheckDownLeftMove(spaces, activePiece) {
   const file = activePiece.id.split('')[0];
   const rank = parseInt(activePiece.id.split('')[1]);
+  let opponent = '';
+  if (activePiece.player === 'white') {
+    opponent = 'black';
+  } else {
+    opponent = 'white';
+  }
 
-  if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`)) {
-    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).children[0].classList[0] === 'black') {
-      possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`);
-      $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).addClass('available');
+  if (activePiece.piece === 'pawn') {
+    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`)) {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).children[0].classList[0] === opponent) {
+        possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`);
+        $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).addClass('available');
+        return false;
+      }
+    }
+  } else {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`)) {
+        if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).children[0].classList[0] === activePiece.player) {
+          return false;
+        } else if (document.getElementById(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).children[0].classList[0] === opponent) {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).addClass('available');
+          return false;
+        } else {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) + spaces]}${rank - spaces}`).addClass('available');
+          return true;
+        }
+      }
     }
   }
-}
 
 function CheckUpRightMove(spaces, activePiece) {
   const file = activePiece.id.split('')[0];
   const rank = parseInt(activePiece.id.split('')[1]);
+  let opponent = '';
+  if (activePiece.player === 'white') {
+    opponent = 'black';
+  } else {
+    opponent = 'white';
+  }
 
-  if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`)) {
-    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).children[0].classList[0] === 'white') {
-      possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`);
-      $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).addClass('available');
+  if (activePiece.piece === 'pawn') {
+    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`)) {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).children[0].classList[0] === opponent) {
+        possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`);
+        $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).addClass('available');
+        return false;
+      }
+    }
+  } else {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`)) {
+        if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).children[0].classList[0] === activePiece.player) {
+          return false;
+        } else if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).children[0].classList[0] === opponent) {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).addClass('available');
+          return false;
+        } else {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank + spaces}`).addClass('available');
+          return true;
+        }
+      }
     }
   }
-}
 
 function CheckUpLeftMove(spaces, activePiece) {
   const file = activePiece.id.split('')[0];
   const rank = parseInt(activePiece.id.split('')[1]);
+  let opponent = '';
+  if (activePiece.player === 'white') {
+    opponent = 'black';
+  } else {
+    opponent = 'white';
+  }
 
-  if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`)) {
-    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).children[0].classList[0] === 'white') {
-      possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`);
-      $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).addClass('available');
+  if (activePiece.piece === 'pawn') {
+    if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`)) {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).children[0].classList[0] === opponent) {
+        possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`);
+        $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).addClass('available');
+        return false;
+      }
+    }
+  } else {
+      if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`)) {
+        if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).children[0].classList[0] === activePiece.player) {
+          return false;
+        } else if (document.getElementById(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).children[0].classList[0] === opponent) {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).addClass('available');
+          return false;
+        } else {
+          possibleMoves.push(`${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`);
+          $(`#${columns[columns.findIndex((element) => element === file) - spaces]}${rank - spaces}`).addClass('available');
+          return true;
+        }
+      }
     }
   }
-}
